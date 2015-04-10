320d95be02dadbc9e02426cec933bf99015297d3
Muhammad Saqib Sarwar <saqibsarwar@gmail.com>
2015-04-10 11:02:14 +0500

    modified text domain

--- quick-and-easy-faqs.php
+++ quick-and-easy-faqs.php
@@ -14,7 +14,7 @@
  * Author URI:        http://saqibsarwar.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       qe-faqs
+ * Text Domain:       quick-and-easy-faqs
  * Domain Path:       /languages
  */
 
