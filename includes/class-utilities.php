53ad93ff83b6097e4f5d0d3a100a951267c58d34
sungraizfaryad <sungraiz@inspirythemes.com>
2019-11-25 16:25:32 +0500

    Utilities class added

---
+++ includes/class-utilities.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * It includes basic methods which will be used across both the
+ * public-facing side of the site and the admin area.
+ */
+
+namespace Quick_And_Easy_Faqs\Includes;
+
+class Utilities {
+
+}
