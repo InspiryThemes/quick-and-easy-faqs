5581b1d451284f1df1fded2efdb111eb292a6da0
sungraizfaryad <sungraiz@inspirythemes.com>
2019-11-25 16:30:31 +0500

    Common method moved into utilities class and frontend class extends it

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -6,7 +6,9 @@
 
 namespace Quick_And_Easy_Faqs\Frontend;
 
-class Frontend {
+use Quick_And_Easy_Faqs\Includes\Utilities;
+
+class Frontend extends Utilities {
 
 	/**
 	 * The ID of this plugin.
@@ -77,26 +79,6 @@ class Frontend {
 			true
 		);
 
-	}
-
-	/**
-	 * Get the value of a settings field
-	 *
-	 * @param string $option settings field name.
-	 * @param string $section the section name this field belongs to.
-	 * @param string $default default text if it's not found.
-	 *
-	 * @return mixed
-	 */
-	public function get_option( $option, $section, $default = '' ) {
-
-		$options = get_option( $section );
-
-		if ( isset( $options[ $option ] ) ) {
-			return $options[ $option ];
-		}
-
-		return $default;
 	}
 
 	/**
