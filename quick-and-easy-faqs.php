3ed5ecbbcf6b10e559036eed41b72a872138a7a9
Fahid Javid <fahidjavid@gmail.com>
2025-12-04 11:52:56 +0500

    Bumped plugin version from 1.3.13 to 1.3.14 & updated other info.

--- quick-and-easy-faqs.php
+++ quick-and-easy-faqs.php
@@ -3,10 +3,10 @@
  * Plugin Name:       Quick and Easy FAQs
  * Plugin URI:        https://wordpress.org/plugins/quick-and-easy-faqs/
  * Description:       A quick and easy way to add FAQs to your site.
- * Version:           1.3.13
- * Tested up to:      6.8.1
+ * Version:           1.3.14
+ * Tested up to:      6.9
  * Requires at least: 6.0
- * Requires PHP:      7.4
+ * Requires PHP:      8.3
  * Author:            InspiryThemes
  * Author URI:        https://inspirythemes.com/
  * License:           GPL-2.0+
